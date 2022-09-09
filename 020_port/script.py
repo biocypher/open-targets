d26b515e6d95e8cb85d10327e0a46612c81d071b
Sebastian Lobentanzer <sebastian.lobentanzer@gmail.com>
2022-09-09 20:58:49 +0200

    add profiling to script

--- 020_port/script.py
+++ 020_port/script.py
@@ -1,5 +1,37 @@
+import cProfile
+import io
+import pstats
+
 from adapter import BioCypherAdapter
 
-adapter = BioCypherAdapter(db_name="ppi1")
+PROFILE = False
 
-adapter.write_to_csv_for_admin_import()
+
+def main():
+    """
+    Run adapter to import data into Neo4j.
+
+    Optionally, run with profiling.
+    """
+    if PROFILE:
+        profile = cProfile.Profile()
+        profile.enable()
+
+    # create and run adapter
+    adapter = BioCypherAdapter(db_name="ppi1")
+    adapter.write_to_csv_for_admin_import()
+
+    if PROFILE:
+        profile.disable()
+
+        s = io.StringIO()
+        sortby = pstats.SortKey.CUMULATIVE
+        ps = pstats.Stats(profile, stream=s).sort_stats(sortby)
+        ps.print_stats()
+
+        ps.dump_stats("adapter.prof")
+        # look at stats using snakeviz
+
+
+if __name__ == "__main__":
+    main()
