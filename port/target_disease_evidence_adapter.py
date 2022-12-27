a522e4602ba5c1efb774683d59e043be3f88a317
Sebastian Lobentanzer <sebastian.lobentanzer@gmail.com>
2022-12-27 19:37:43 +0100

    start on target-disease adapter

---
+++ port/target_disease_evidence_adapter.py
@@ -0,0 +1,104 @@
+import os
+from pyspark.sql import SparkSession
+from enum import Enum
+
+class TargetDiseaseDataset(Enum):
+    CANCER_BIOMARKERS = "cancer_biomarkers"
+    CANCER_GENE_CENSUS = "cancer_gene_census"
+    CHEMBL = "chembl"
+    CLINGEN = "clingen"
+    CRISPR = "crispr"
+    EUROPE_PMC = "europepmc"
+    EVA = "eva"
+    EVA_SOMATIC = "eva_somatic"
+    EXPRESSION_ATLAS = "expression_atlas"
+    GENOMICS_ENGLAND = "genomics_england"
+    GENE_BURDEN = "gene_burden"
+    GENE2PHENOTYPE = "gene2phenotype"
+    IMPACT = "impc"
+    INTOGEN = "intogen"
+    ORPHANET = "orphanet"
+    OT_GENETICS_PORTAL = "ot_genetics_portal"
+    PROGENY = "progeny"
+    REACTOME = "reactome"
+    SLAP_ENRICH = "slapenrich"
+    SYSBIO = "sysbio"
+    UNIPROT_VARIANTS = "uniprot_variants"
+    UNIPROT_LITERATURE = "uniprot_literature"
+
+class TargetDiseaseNodeField(Enum):
+    DISEASE_ACCESSION = "diseaseId"
+    TARGET_GENE_ENSG = "targetId"
+
+class TargetDiseaseEvidenceAdapter:
+    def __init__(
+        self, 
+        datasets: list[Enum] = None,
+        node_fields: list[Enum] = None, 
+        edge_fields: list = None
+    ):
+        self.datasets = datasets
+        self.node_fields = node_fields
+        self.edge_fields = edge_fields
+
+        if not self.datasets:
+            raise ValueError("datasets must be provided")
+
+        if not self.node_fields:
+            raise ValueError("node_fields must be provided")
+
+        if not self.edge_fields:
+            raise ValueError("edge_fields must be provided")
+        
+        # Create SparkSession
+        self.spark = SparkSession.builder \
+            .master("local") \
+            .appName("port") \
+            .getOrCreate()
+
+    def load_data(self, stats: bool = False):
+
+        # Read in evidence data
+        self.path = "data/ot_files/evidence"
+        self.full_df = self.spark.read.parquet(self.path)
+
+        if stats:
+
+            # print schema
+            print(self.full_df.printSchema())
+
+            # print number of rows
+            print(f"Length of evidence data: {self.full_df.count()} entries")
+
+            # print number of rows per datasource
+            self.full_df.groupBy("datasourceId").count().show(100)
+
+    def show_datasources(self):
+
+        # collect all distinct datasourceId values
+        datasources = self.full_df.select("datasourceId").distinct().collect()
+
+        # convert to list
+        self.datasources = [x.datasourceId for x in datasources]
+        print(self.datasources)
+
+    def get_nodes(self):
+
+        # Select columns of interest
+        self.node_df = self.full_df.select("datasourceId", "targetId", "diseaseId")
+        
+        for datasource in [field.value for field in self.datasets]:
+            
+            df = self.spark.read.parquet(self.path).where(f"datasourceId = '{datasource}'")
+            df.select("targetId", "diseaseId")
+
+            # get distinct values for each column
+            target_ids = df.select("targetId").distinct().collect()
+            disease_ids = df.select("diseaseId").distinct().collect()
+
+            # yield target and disease ids
+            for target_id in target_ids:
+                yield target_id.targetId
+
+            for disease_id in disease_ids:
+                yield disease_id.diseaseId
---
+++ port/target_disease_script.py
@@ -0,0 +1,32 @@
+from target_disease_evidence_adapter import TargetDiseaseEvidenceAdapter, TargetDiseaseDataset, TargetDiseaseNodeField
+import biocypher
+
+datasets = [
+    TargetDiseaseDataset.SYSBIO,
+    TargetDiseaseDataset.PROGENY,
+]
+
+node_fields = [
+    TargetDiseaseNodeField.DISEASE_ACCESSION,
+    TargetDiseaseNodeField.TARGET_GENE_ENSG,
+]
+
+def main():
+
+    driver = biocypher.Driver(
+        user_schema_config_path="config/target_disease_schema_config.yaml",
+    )
+
+    adapter = TargetDiseaseEvidenceAdapter(
+        datasets=datasets,
+        node_fields=node_fields,
+        edge_fields="None",
+    )
+
+    adapter.load_data()
+
+    driver.write_nodes(adapter.get_nodes())
+
+if __name__ == "__main__":
+    main()
+
