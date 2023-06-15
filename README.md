3aee5e3dd8bc804a6edcda28edb4f2de7bf02806
Sebastian Lobentanzer <sebastian.lobentanzer@gmail.com>
2023-06-15 11:17:31 +0200

    download script

--- README.md
+++ README.md
@@ -46,7 +46,9 @@ ontology`, `Target - mouse phenotypes` and `Target - Disease Evidence`. CAVE:
 The latter, which is the main source of target-disease interactions in the open
 targets platform, is provided in two links, one for the literature evidence
 (`literature/evidence`) and one for the full aggregated set (simply `evidence`).
-The adapter uses the full set, so make sure to download the correct one.
+The adapter uses the full set, so make sure to download the correct one. The
+scripts directory contains a `parquet_download.sh` script that can be used to
+download the files (make sure to execute it in the correct folder).
 
 To transfer the columnar data to a knowledge graph, we use the adapter in
 `adapters/target_disease_evidence_adapter.py`, which is called from the script
---
+++ scripts/parquet_download.sh
@@ -0,0 +1,6 @@
+wget --recursive --no-parent --no-host-directories --cut-dirs 8 ftp://ftp.ebi.ac.uk/pub/databases/opentargets/platform/23.02/output/etl/parquet/targets
+wget --recursive --no-parent --no-host-directories --cut-dirs 8 ftp://ftp.ebi.ac.uk/pub/databases/opentargets/platform/23.02/output/etl/parquet/diseases
+wget --recursive --no-parent --no-host-directories --cut-dirs 8 ftp://ftp.ebi.ac.uk/pub/databases/opentargets/platform/23.02/output/etl/parquet/molecule
+wget --recursive --no-parent --no-host-directories --cut-dirs 8 ftp://ftp.ebi.ac.uk/pub/databases/opentargets/platform/23.02/output/etl/parquet/evidence
+wget --recursive --no-parent --no-host-directories --cut-dirs 8 ftp://ftp.ebi.ac.uk/pub/databases/opentargets/platform/23.02/output/etl/parquet/go
+wget --recursive --no-parent --no-host-directories --cut-dirs 8 ftp://ftp.ebi.ac.uk/pub/databases/opentargets/platform/23.02/output/etl/parquet/mousePhenotypes
