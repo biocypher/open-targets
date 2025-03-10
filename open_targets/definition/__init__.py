fc3dba52e2c27855d6fea4f9e5a695dfae831971
Paul TO <kptoemail@gmail.com>
2025-03-10 16:49:29 +0000

    Exports all provided definitions

---
+++ open_targets/definition/__init__.py
@@ -0,0 +1,21 @@
+from open_targets.definition.edge_target_disease import edge_target_disease
+from open_targets.definition.edge_target_go import edge_target_go
+from open_targets.definition.node_diseases import node_diseases
+from open_targets.definition.node_gene_ontology import node_gene_ontology
+from open_targets.definition.node_molecule import node_molecule
+from open_targets.definition.node_mouse_model import node_mouse_model
+from open_targets.definition.node_mouse_phenotype import node_mouse_phenotype
+from open_targets.definition.node_mouse_target import node_mouse_target
+from open_targets.definition.node_targets import node_targets
+
+__all__ = [
+    "edge_target_disease",
+    "edge_target_go",
+    "node_diseases",
+    "node_gene_ontology",
+    "node_molecule",
+    "node_mouse_model",
+    "node_mouse_phenotype",
+    "node_mouse_target",
+    "node_targets",
+]
